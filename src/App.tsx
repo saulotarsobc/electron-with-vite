db9ab72b74109ee81790966dfb1e3cc6c9f707bb
Saulo Costa <saulotarsobc@gmail.com>
2025-06-14 08:51:43 -0300

    refactor(pages): simplify dashboard and home components
    - Move Dashboard component from components to pages directory
    - Remove complex system monitor dashboard implementation
    - Simplify Home page by removing unnecessary logos and functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import { Route, HashRouter as Router, Routes } from "react-router-dom";
-import { Dashboard } from "./components/Dashboard";
 import Header from "./components/Header";
 import "./components/Header.css";
 import About from "./pages/About";
 import Home from "./pages/Home";
+import Dashboard from "./pages/Dashboard";
 
 function App() {
   return (
